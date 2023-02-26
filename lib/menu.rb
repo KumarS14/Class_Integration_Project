88b575f2f51a88ba542eadbfdcccbb1c21b22f4e
KumarS14 <sonjaykumar1404@gmail.com>
2023-02-26 15:17:01 +0000

    added dishes to order

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,14 +1,15 @@
 class Menu
-    def initialize
-    @menu = [
-        "chips" =>  2,
+    def initialize(menu)
+    @menu = menu
+    @dishes = (["chips" =>  2,
         "burger" => 4,
         "kebab" => 5.75,
         "mac and cheese" => 4.47
     ]
+    )
     end
     def the_menu
-         @menu
+      return @dishes
     end
     def show_menu
          the_menu.each do |key|
