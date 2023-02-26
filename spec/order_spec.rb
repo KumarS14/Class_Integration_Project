8255a1e25581e949c7700a398131b6f96e27d81d
KumarS14 <sonjaykumar1404@gmail.com>
2023-02-26 13:15:30 +0000

    error test so if there is no input it will raise

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,9 +1,15 @@
 require "order.rb"
 RSpec.describe Order do
     context "User orders food using method" do
+        it "should return nil" do
+            orders = Order.new
+            expect {orders.to_order("")}.to raise_error "You have not entered a order"
+        end
         it "should return chips" do
             orders = Order.new
-            expect(orders.show_order("chips")).to eq "chips"
+            orders.to_order("burger")
+            orders.to_order("chips")
+            expect(orders.show_order).to eq "burger, chips"
         end
     end
 end
