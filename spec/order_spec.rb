7f8f9a4c51483ea1217907dde61586f4c620beed
KumarS14 <sonjaykumar1404@gmail.com>
2023-02-26 13:19:50 +0000

    test to return 3 items

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -5,11 +5,27 @@ RSpec.describe Order do
             orders = Order.new
             expect {orders.to_order("")}.to raise_error "You have not entered a order"
         end
-        it "should return chips" do
+        it "should return chips and burger" do
             orders = Order.new
             orders.to_order("burger")
             orders.to_order("chips")
             expect(orders.show_order).to eq "burger, chips"
         end
+        it "should return chips and burger and kebab" do
+            orders = Order.new
+            orders.to_order("burger")
+            orders.to_order("chips")
+            orders.to_order("kebab")
+            expect(orders.show_order).to eq "burger, chips, kebab"
+        end
+        it "should return chips and burger and kebab" do
+            orders = Order.new
+            orders.to_order("burger")
+            orders.to_order("chips")
+            orders.to_order("kebab")
+            orders.to_order("mac and cheese")
+            expect(orders.show_order).to eq "burger, chips, kebab, mac and cheese"
+        end
+
     end
 end
