77608f349ef581b9e3679d37de00b4abf3242891
KumarS14 <sonjaykumar1404@gmail.com>
2023-02-26 19:08:08 +0000

    turned menu from array to hash

--- lib/order.rb
+++ lib/order.rb
@@ -1,19 +1,27 @@
 class Order 
     def initialize
         @order = []
-        @menu = []
+        menu = Hash.new
     end
     def all(menu)
-        @menu << menu
-    end
-    def outputs
-        return @menu
+        @menu = menu.the_menu
     end
 
+    def show_menu
+        return @menu
+    end
+ 
     def to_order(order)
         fail "You have not entered a order" if order == ""
-        @order.push(order)
+       @order.push(order)
     end
+    def price
+       show_menu.each do |key|
+        return key["burger"]
+       end
+
+    end
+
     def show_order
         return @order.join(", ")
     end
