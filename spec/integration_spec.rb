04ccf58ac4c704d8e24c02044d2ca9e01ed1023f
KumarS14 <sonjaykumar1404@gmail.com>
2023-02-26 19:07:52 +0000

    itehration test to see if key returns value

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -9,6 +9,31 @@ RSpec.describe "Integration" do
             "mac and cheese" => 4.47
         ])
         order.all(menu)
-        expect(order.outputs).to eq [menu]
-        end
+        expect(order.show_menu).to eq  [{"burger"=>4, "chips"=>2, "kebab"=>5.75, "mac and cheese"=>4.47}]
+    end
+
+    it "tests to see if order is valid" do
+        order = Order.new
+        menu = Menu.new([
+            {"chips" =>  2},
+            {"burger" => 4},
+            {"kebab" => 5.75},
+            {"mac and cheese" => 4.47}
+        ])
+        order.all(menu)
+        order.to_order("burger")
+        expect(order.show_order).to eq "burger"
+    end
+    it "tests to see if price is returned" do
+        order = Order.new
+        menu = Menu.new({
+            "chips" =>  2,
+            "burger" => 4,
+            "kebab" => 5.75,
+            "mac and cheese" => 4.47
+        }
+        )
+        order.all(menu)
+        expect(order.price).to eq 4
+    end
 end
