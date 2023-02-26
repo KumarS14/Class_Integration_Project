44cd365fde747548021428da382638055566e96c
KumarS14 <sonjaykumar1404@gmail.com>
2023-02-26 11:52:50 +0000

    tests to see if hash outputs food and items

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe Menu do
     context "menu has list of items" do
         it "returns chips 2 as a basic test" do
             menu = Menu.new()
-            expect(menu.show_menu).to eq "chips 2"
+            expect(menu.show_menu).to eq ["burger" => 4, "chips" => 2,]
         end
         
     end
