09e7902bcc18130311a1045ea59f607cfdeed712
KumarS14 <sonjaykumar1404@gmail.com>
2023-02-26 13:15:44 +0000

    code to raise fail if nil

--- lib/order.rb
+++ lib/order.rb
@@ -2,7 +2,11 @@ class Order
     def initialize
         @order = []
     end
-    def show_order(order)
-        @order.push(order).join
+    def to_order(order)
+        fail "You have not entered a order" if order == ""
+        @order.push(order)
+    end
+    def show_order
+        return @order.join(", ")
     end
 end 
