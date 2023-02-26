def4e7fc13461fbc359864c2be4559b368ced539
KumarS14 <sonjaykumar1404@gmail.com>
2023-02-26 13:20:03 +0000

    creates instance of menu

--- lib/order.rb
+++ lib/order.rb
@@ -2,6 +2,9 @@ class Order
     def initialize
         @order = []
     end
+    def add(menu)
+        @menu = menu 
+    end
     def to_order(order)
         fail "You have not entered a order" if order == ""
         @order.push(order)
