f95aa567a55cadab0d94322f1a87097edbf64eb6
KumarS14 <sonjaykumar1404@gmail.com>
2023-02-26 15:17:15 +0000

    put hash into a array

--- lib/order.rb
+++ lib/order.rb
@@ -1,10 +1,15 @@
 class Order 
     def initialize
         @order = []
+        @menu = []
     end
-    def add(menu)
-        @menu = menu 
+    def all(menu)
+        @menu << menu
     end
+    def outputs
+        return @menu
+    end
+
     def to_order(order)
         fail "You have not entered a order" if order == ""
         @order.push(order)
